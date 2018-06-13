b7af63f31ab1e9776e8dabaf0a174856d8eef7e4
guochunyuan@gitv.cn <guochunyuan@gitv.cn>
2018-06-13 15:46:03 +0800

    update

--- demo/test/ObjectTest.lua
+++ demo/test/ObjectTest.lua
@@ -5,4 +5,86 @@
 -- Time: 11:50
 -- To change this template use File | Settings | File Templates.
 --
+-- Meta class
+Rectangle = {area = 0, length = 0, breadth = 0}
 
+-- 派生类的方法 new
+function Rectangle:new (o,length,breadth)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.length = length or 0
+    self.breadth = breadth or 0
+    self.area = length*breadth;
+    return o
+end
+
+-- 派生类的方法 printArea
+function Rectangle:printArea ()
+    print("矩形面积为 ",self.area)
+end
+
+
+r = Rectangle:new(nil,10,20)
+
+--访问函数的方式；
+r:printArea()
+
+
+
+-- Meta class
+Shape = {area = 0}
+-- 基础类方法 new
+function Shape:new (o,side)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    side = side or 0
+    self.area = side*side;
+    return o
+end
+-- 基础类方法 printArea
+function Shape:printArea ()
+    print("面积为 ",self.area)
+end
+
+-- 创建对象
+myshape = Shape:new(nil,10)
+myshape:printArea()
+
+Square = Shape:new()
+-- 派生类方法 new
+function Square:new (o,side)
+    o = o or Shape:new(o,side)
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+-- 派生类方法 printArea
+function Square:printArea ()
+    print("正方形面积为 ",self.area)
+end
+
+-- 创建对象
+mysquare = Square:new(nil,10)
+mysquare:printArea()
+
+Rectangle = Shape:new()
+-- 派生类方法 new
+function Rectangle:new (o,length,breadth)
+    o = o or Shape:new(o)
+    setmetatable(o, self)
+    self.__index = self
+    self.area = length * breadth
+    return o
+end
+
+-- 派生类方法 printArea
+function Rectangle:printArea ()
+    print("矩形面积为 ",self.area)
+end
+
+-- 创建对象
+myrectangle = Rectangle:new(nil,10,20)
+myrectangle:printArea()
--- demo/test/functiontest.lua
+++ demo/test/functiontest.lua
@@ -6,6 +6,7 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+
 --[[ 函数返回两个值的最大值 --]]
 function max(num1, num2)
 
@@ -21,3 +22,16 @@ end
 print("两值比较最大值为 ", max(10, 4))
 print("两值比较最大值为 ", max(5, 6))
 
+function maximum (a)
+    local mi = 1             -- 最大值索引
+    local m = a[mi]          -- 最大值
+    for i,val in ipairs(a) do
+        if val > m then
+            mi = i
+            m = val
+        end
+    end
+    return m, mi
+end
+
+print(maximum({8,10,23,12,5}))
